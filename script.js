4be7595b8f5912078446b7b48e27f6d77fcfc327
niellbinn92 <niellbinn92@gmail.com>
2026-09-07 16:37:23 +0700

    Update APPS_SCRIPT_URL to new endpoint

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ const API_BASE = "https://nielofc-github-io.vercel.app";
 const SHEET_CSV = "https://docs.google.com/spreadsheets/d/1dTfloE3c-TbWMqTk6U42pnbil4hsTzpnvjNVEdA0oyA/export?format=csv";
 
 // URL APPS SCRIPT TERBARU
-const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyEhcmJGy7lrYwO-rHPeX3iC8eMSU4QNRkjzJEZIlV6au1dcL5K5yjsR6OB394Lt7A9/exec";
-
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyic58FgbGb-IER0FWLVGXvZegVZ67TLaRbWoL9I4aPHTUMVrcS6W91Bbj4gR4rrx_6/exec";
 const PRODUCT_IMAGES = {
   "DRIP APKMOD": "https://i.ibb.co.com/zWBMST9S/9659b485-a457-42af-a695-5ea681df4c6c.jpg",
   "DRIP PROXY": "https://i.ibb.co.com/zWBMST9S/9659b485-a457-42af-a695-5ea681df4c6c.jpg",

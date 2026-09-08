293bb1a928ae9ba63974c436400260708c8c9b44
niellbinn92 <niellbinn92@gmail.com>
2026-09-08 18:04:32 +0700

    Update SHEET_CSV URL in script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const API_BASE = "https://nielofc-github-io.vercel.app";
-const SHEET_CSV = "https://docs.google.com/spreadsheets/d/1dTfloE3c-TbWMqTk6U42pnbil4hsTzpnvjNVEdA0oyA/export?format=csv";
+const SHEET_CSV = "https://docs.google.com/spreadsheets/d/1haCbzfPKJ06opxQYvtDzE2ueBrCFb7zPGrBlrzNfBLY/export?format=csv";
 const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyfR2H-sVmb8NxDMFtnJXd-Td8o4VFCgLCCgFBf6_bMJ_8vx-UAhpZuhdH-KyEYoOq3GQ/exec";
 const PRODUCT_IMAGES = {
   "DRIP APKMOD": "https://i.ibb.co.com/zWBMST9S/9659b485-a457-42af-a695-5ea681df4c6c.jpg",
